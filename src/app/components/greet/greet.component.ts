d4c4e9b07462958d27d0087cb74537df801923b3
Carly Morris <thabai@hotmail.com>
2021-08-30 20:32:02 +0100

    add button

---
+++ src/app/components/greet/greet.component.ts
@@ -0,0 +1,21 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-greet',
+//   templateUrl: './greet.component.html',
+//   styleUrls: ['./greet.component.css']
+// })
+// export class GreetComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+//   name: String = "Carly";
+
+//   greet(): void {
+//     alert("Hello " + this.name);
+//   };
+
+// }
